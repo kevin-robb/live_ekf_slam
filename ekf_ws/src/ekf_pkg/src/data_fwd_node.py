Change prediction jacobian. Improves results and seems to prevent all the landmarks randomly moving and swirling around which they shouldnt be allowed to do, but I don't think it's technically correct now.

ekf_ws/src/ekf_pkg/src/data_fwd_node.py
@@ -16,7 +16,7 @@ import numpy as np
 
 ############ GLOBAL VARIABLES ###################
 USE_RSS_DATA = False # T = use demo set. F = randomize map and create new traj.
-DT = 0.5 # timer period used if cmd line param not provided.
+DT = 0.05 # timer period used if cmd line param not provided.
 odom_pub = None; lm_pub = None; true_map_pub = None; true_pose_pub = None
 pkg_path = None # filepath to this package.
 # Default map from RSS demo:
@@ -197,11 +197,16 @@ def generate_data():
         if abs(hdg) > ODOM_TH_MAX:
             # cap magnitude but keep sign.
             hdg = ODOM_TH_MAX * np.sign(hdg)
+        # add noise.
+        d = d + 2*V[0,0]*random()-V[0,0]
+        hdg = hdg + 2*V[1,1]*random()-V[1,1]
         # update veh position given this odom cmd.
         x_v = [x_v[0] + d*cos(x_v[2]), x_v[1] + d*sin(x_v[2]), x_v[2] + hdg]
         # add noise to odom and add to trajectory.
-        odom_dist.append(d + 2*V[0,0]*random()-V[0,0])
-        odom_hdg.append(hdg + 2*V[1,1]*random()-V[1,1])
+        odom_dist.append(d)
+        odom_hdg.append(hdg)
+        # odom_dist.append(d + 2*V[0,0]*random()-V[0,0])
+        # odom_hdg.append(hdg + 2*V[1,1]*random()-V[1,1])
 
     #####################################################
     # Optionally replace odom with the demo for debugging.
@@ -258,7 +263,7 @@ def generate_data():
 
     ############### SEND DATA ################
     # Publish ground truth of map and veh pose for plotter.
-    rospy.logwarn("Waiting to publish ground truth for plotter.")
+    rospy.loginfo("Waiting to publish ground truth for plotter.")
     rospy.sleep(1)
     true_pose_msg = Float32MultiArray()
     true_pose_msg.data = sum(pos_true, [])

ekf_ws/src/ekf_pkg/src/ekf_node.py
@@ -15,15 +15,15 @@ from geometry_msgs.msg import Vector3
 import sys
 
 ############ GLOBAL VARIABLES ###################
-DT = 0.5 # timer period used if cmd line param not provided.
+DT = 0.05 # timer period used if cmd line param not provided.
 state_pub = None
 ############## NEEDED BY EKF ####################
 # Process noise in (forward, angular). Assume zero mean.
 v_d = 0; v_th = 0
-V = np.array([[0.02**2,0.0],[0.0,0.5*pi/180**2]])
+V = np.array([[0.02**2,0.0],[0.0,(0.5*pi/180)**2]])
 # Sensing noise in (range, bearing). Assume zero mean.
 w_r = 0; w_b = 0
-W = np.array([[0.1**2,0.0],[0.0,1*pi/180**2]])
+W = np.array([[0.1**2,0.0],[0.0,(1*pi/180)**2]])
 # Initial vehicle state mean and covariance.
 x0 = np.array([[0.0],[0.0],[0.0]])
 P0 = np.array([[0.01**2,0.0,0.0],[0.0,0.01**2,0.0],[0.0,0.0,0.005**2]])
@@ -56,7 +56,8 @@ def ekf_iteration(event):
     F_xv = np.array([[1,0,-d_d*sin(x_t[2,0])],
                      [0,1,d_d*cos(x_t[2,0])],
                      [0,0,1]])
-    F_x = np.eye(x_t.shape[0])
+    F_x = np.eye(x_t.shape[0]) * 0
+    # F_x = np.zeros((x_t.shape[0],x_t.shape[0]))
     F_x[0:3,0:3] = F_xv
     F_vv = np.array([[cos(x_t[2,0]), 0],
                      [sin(x_t[2,0]),0],
@@ -125,13 +126,15 @@ def ekf_iteration(event):
                 # form insertion jacobian.
                 Y = np.eye(n+2)
                 Y[n:n+2,n:n+2] = G_z
-                # Y[n:n+2,0:3] = G_x
+                Y[n:n+2,0:3] = G_x
 
                 # update covariance.
                 P_pred = Y @ np.vstack([np.hstack([P_pred, np.zeros((n,2))]), np.hstack([np.zeros((2,n)), W])]) @ Y.T
                 
     # finalize the state update. this works even if no landmarks were detected.
     x_t = x_pred; P_t = P_pred
+    # cap heading to (-pi,pi).
+    x_t[2,0] = remainder(x_t[2,0], tau)
     # publish the current cov + state.
     """
     NOTE this uses a format that gives us only what we need for plotting.